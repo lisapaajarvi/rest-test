dcf9fa282333908aece50c3e7369e85819c42fa1
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 11:48:44 +0200

    static

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'))
+
 app.get('/', (req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
