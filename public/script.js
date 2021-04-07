9d1c0b8d5442fb88a96a9d51d4299e2167fb2210
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 16:01:56 +0200

    small fix

--- public/script.js
+++ public/script.js
@@ -7,5 +7,5 @@ async function getVisitorCount() {
     const response = await fetch('/visitors');
     const visitors = await response.json();
     const visitorDiv = document.getElementById('visitor-div');
-    visitorDiv.innerHTML = visitors;
+    visitorDiv.innerHTML = visitors + ' besökare har varit här'
 }
