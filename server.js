90e0b08141f694bb1e3826b6f2cddee0e9235689
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 14:50:44 +0200

    added visitor count

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+let visitors = 1;
 
 app.use(express.json());
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/visitors', (req, res) => {
+    res.json(visitors)
+})
+
+app.post('/visitors', (req, res) => {
+    visitors ++;
+    res.json(visitors)
+})
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`))
