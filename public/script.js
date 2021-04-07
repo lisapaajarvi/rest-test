4f08e193a017f34fab98ad87882a8a89746b4f72
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-04-07 15:59:20 +0200

    fixed visitor count

---
+++ public/script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', getVisitorCount);
+
+
+async function getVisitorCount() {
+    const response = await fetch('/visitors');
+    const visitors = await response.json();
+    const visitorDiv = document.getElementById('visitor-div');
+    visitorDiv.innerHTML = visitors;
+}
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const port = 3000;
 const app = express();
-let visitors = 1;
+let visitors = 0;
 
 app.use(express.json());
+
+app.get('/',(req, res, next) => {
+    visitors +=1;
+    console.log(visitors);
+    next()
+})
+
 app.use(express.static('public'))
 
 app.get('/visitors', (req, res) => {
-    res.json(visitors)
-})
-
-app.post('/visitors', (req, res) => {
-    visitors ++;
     res.json(visitors)
 })
 
